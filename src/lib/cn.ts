a691e77defb81039c65b13d6ef3d0ee8a453d8aa
Plynexa <plynexa@gmail.com>
2026-08-30 18:10:44 -0300

    Add files via upload

--- src/lib/cn.ts
+++ src/lib/cn.ts
@@ -1,9 +1,6 @@
 import { ClassValue, clsx } from "clsx";
-import { bg_header } from "reportlab/lib/colors";
-import { cls } from "spacy/lang/fr";
 import { tailwindMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  // Simple fallback/shim if tailwind-merge has issues or just use standard merge
-  return inputs.filter(Boolean).join(" ");
+  return tailwindMerge(clsx(inputs));
 }
