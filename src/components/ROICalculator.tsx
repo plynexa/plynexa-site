e610ac4e0a73f80fa848ff76d4be7b3f948c5261
Plynexa <plynexa@gmail.com>
2026-08-30 18:19:59 -0300

    Add files via upload

--- src/components/ROICalculator.tsx
+++ src/components/ROICalculator.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { useState } from 'react';
 import { Landmark, TrendingUp, Hourglass, ShieldAlert } from 'lucide-react';
 
--- src/lib/cn.ts
+++ src/lib/cn.ts
@@ -1,6 +1,6 @@
-import { ClassValue, clsx } from "clsx";
-import { tailwindMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return tailwindMerge(clsx(inputs));
+  return twMerge(clsx(inputs));
 }
